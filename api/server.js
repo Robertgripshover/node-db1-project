8bd6f48ada4be8ff1ac5a0f0e2ec546883c2f5f9
Robert Gripshover <robertgrips@gmail.com>
2023-03-17 16:06:01 -0400

    m

--- api/server.js
+++ api/server.js
@@ -1,7 +1,24 @@
 const express = require("express");
 
+const accountsRouter = require('./accounts/accounts-router') //added this in for the accountRouter
+
 const server = express();
 
 server.use(express.json());
+
+
+
+
+server.use('/api/accounts', accountsRouter) //this needs to be put in this spot below express.json thing
+//and about the server.use('*')
+
+
+
+
+server.use('*', (req, res) => {
+    res.status(404).json({
+        message: 'not found'
+    })
+}) //always put at the bottom
 
 module.exports = server;
