1682dc551de9dea0edb9b1c6fac274f8f2d7891b
Robert Gripshover <robertgrips@gmail.com>
2023-03-20 15:18:41 -0400

    wip

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -7,39 +7,36 @@ exports.checkAccountPayload = (req, res, next) => {
   const { name, budget } = req.body //descruturing out the name and budget from the body of the request
   if (name === undefined || budget === undefined) {
     errorMessage.message = 'name and budget are required'
-    next(errorMessage) 
-
-    //this will spit out the error message if this condition is not met
+    
     //if the condition IS met then it will move onto the next else if and keep checking
 
   } else if (typeof name !== 'string') {
     errorMessage.message = 'name of account must be a string'
-    next(errorMessage)
-
-    //this will spit out the error message if this condition is not met
+  
     //if the condition IS met then it will move onto the next else if and keep checking
 
   } else if (name.trim().length < 3 || name.trim().length > 100) {
     errorMessage.message = 'name of account must be between 3 and 100'
-    next(errorMessage)
-
-    //this will spit out the error message if this condition is not met
+  
     //if the condition IS met then it will move onto the next else if and keep checking
 
   } else if (typeof budget !== 'number' || isNaN(budget)) {
     errorMessage.message = 'budget of account must be a number'
-    next(errorMessage)
-
-    //this will spit out the error message if this condition is not met
+  
     //if the condition IS met then it will move onto the next else if and keep checking
 
   } else if (budget < 0 || budget > 1000000) {
     errorMessage.message = 'budget of account is to large or to small'
-    next(errorMessage)
-
-    //this will spit out the error message if this condition is not met
+  
     //if the condition IS met then it will move onto the next else if and keep checking
 
+  }
+
+  if (errorMessage.message) {
+    next(errorMessage) //this will run if there is an error anywhere in the code
+    //and it will spit out the specific error message
+  } else {
+    next()
   }
   
 }
